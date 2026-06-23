64823de2a1ec0bef8a199c9d5785832d579d5216
SujaanBhalla <bhallasujaan@gmail.com>
2026-06-24 00:55:25 +0530

    Add Day 3 Treasure Island project

---
+++ treasure_island.py
@@ -0,0 +1,39 @@
+# Lecture 28 - Treasure Island Project
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+choice1 = input("Left or Right?\n").lower()
+
+if choice1 == "left":
+
+    choice2 = input("Swim or Wait?\n").lower()
+
+    if choice2 == "wait":
+
+        choice3 = input("Red, Yellow or Blue?\n").lower()
+
+        if choice3 == "yellow":
+            print("You Win!")
+
+        elif choice3 == "red":
+            print("Game Over.")
+
+        elif choice3 == "blue":
+            print("Game Over.")
+
+        else:
+            print("Game Over.")
+
+    else:
+        print("Game Over.")
+
+else:
+    print("Game Over.")
+
+# Learned:
+# if, elif, else
+# Nested conditions
+# .lower()
+# Input handling
+# Decision-based game logic
